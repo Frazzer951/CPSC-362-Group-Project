12ec69235d4228f54c39482936410c11712b7139
Frazzer951 <luke343279@gmail.com>
2022-09-15 15:57:31 -0700

    update tables

--- backend/db_create.sql
+++ backend/db_create.sql
@@ -1,24 +1,25 @@
 
 CREATE TABLE Users (
     user_id INT NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    about_me VARCHAR(30),
+    username VARCHAR(32) NOT NULL
+    password VARCHAR(32) NOT NULL,
+    about_me VARCHAR(1024),
     admin BOOL,
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE Threads (
     thread_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(30),
-    description VARCHAR(50)
+    name VARCHAR(128),
+    description VARCHAR(128)
 );
 
 CREATE TABLE Posts(
     post_id INT NOT NULL,
     user_id INT NOT NULL,
     thread_id INT NOT NULL,
-    title VARCHAR(30),
-    body VARCHAR(50),
+    title VARCHAR(128),
+    body VARCHAR(1024),
     PRIMARY KEY (post_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (thread_id) REFERENCES Threads(thread_id)
@@ -28,7 +29,7 @@ CREATE TABLE Comments(
     comment_id INT NOT NULL,
     user_id INT NOT NULL,
     post_id INT NOT NULL,
-    body VARCHAR(50),
+    body VARCHAR(1024),
     PRIMARY KEY (comment_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (post_id) REFERENCES Posts(post_id)
