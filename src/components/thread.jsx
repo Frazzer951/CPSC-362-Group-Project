ae9d7a029f22f47b110c91e8d506bd05265d70a7
Luke Eltiste <luke343279@gmail.com>
2022-09-19 15:29:14 -0700

    update threads to use api

--- src/components/thread.jsx
+++ src/components/thread.jsx
@@ -5,13 +5,13 @@ export default function Thread(props) {
   let { thread } = props;
 
   return (
-    <Link style={{ textDecoration: "none" }} to={`/${thread.id}`} key={thread.id}>
+    <Link style={{ textDecoration: "none" }} to={`/${thread.thread_id}`} key={thread.thread_id}>
       <Card sx={{ minWidth: 275, margin: "0.5rem" }}>
         <CardContent>
           <Typography variant="h5" component="div">
             {thread.name}
           </Typography>
-          <Typography variant="body2">{thread.desc}</Typography>
+          <Typography variant="body2">{thread.description}</Typography>
         </CardContent>
       </Card>
     </Link>
--- src/routes/threads.jsx
+++ src/routes/threads.jsx
@@ -1,20 +1,40 @@
+import { useEffect, useState } from "react";
 import { Box, Divider, Typography } from "@mui/material";
-import { getThreads } from "../data";
+import { api } from "../api";
 import Thread from "../components/thread";
 
 export default function Threads() {
-  let threads = getThreads();
+  const [threads, setThreads] = useState();
+
+  useEffect(() => {
+    api
+      .get("/threads")
+      .then((res) => {
+        console.log(res);
+        setThreads(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <Typography variant="h4" sx={{ padding: "1rem" }}>
         Threads
       </Typography>
+
       <Divider />
-      <Box>
-        {threads.map((thread) => (
-          <Thread thread={thread} />
-        ))}
-      </Box>
+
+      {threads ? (
+        <Box>
+          {threads.map((thread) => (
+            <Thread thread={thread} />
+          ))}
+        </Box>
+      ) : (
+        <h2>Loading</h2>
+      )}
     </div>
   );
 }
