24b79b3935bd18239d99c4e47373182e907b027e
Luke Eltiste <luke343279@gmail.com>
2022-09-20 10:56:21 -0700

    setup basic stuff for login

---
+++ src/components/login.jsx
@@ -0,0 +1,95 @@
+import { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "../context/AuthProvider";
+
+import axios from "../api/axios";
+const LOGIN_URL = "/auth";
+
+/// Code from tutorial at https://www.youtube.com/watch?v=X3qyxo_UTR4
+export default function Login() {
+  const { setAuth } = useContext(AuthContext);
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(LOGIN_URL, {
+        username: user,
+        password: pwd,
+        withCredentials: true,
+      });
+
+      console.log(response?.data);
+
+      const userID = response?.data?.userID;
+      const admin = response?.data?.isAdmin;
+      setAuth({ user, pwd, admin, userID });
+
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+    }
+  };
+
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
+
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" onChange={(e) => setPwd(e.target.value)} value={pwd} required />
+
+            <button>Sign In</button>
+          </form>
+
+          <p>
+            Need an Account?
+            <br />
+            <span className="line">
+              {/* put router link here */}
+              <a href="#">Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
+  );
+}
