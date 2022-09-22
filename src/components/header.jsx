e3f3fbdc2bacb67a3914a66874d1c69c4e22c08e
Frazzer951 <luke343279@gmail.com>
2022-09-22 15:51:51 -0700

    modal when clicking on avatar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,25 @@
 import { AccountCircle } from "@mui/icons-material";
-import { AppBar, Avatar, Box, Button, Container, Toolbar, Typography } from "@mui/material";
+import { AppBar, Avatar, Box, Button, Container, Toolbar, Typography, Modal } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -31,9 +48,27 @@ export default function Header() {
             </Link>
           </Box>
 
-          <Avatar sx={{ bgcolor: "#ffffff" }}>
-            <AccountCircle color="secondary" />
-          </Avatar>
+          <Button onClick={handleOpen}>
+            <Avatar sx={{ bgcolor: "#ffffff" }}>
+              <AccountCircle color="secondary" />
+            </Avatar>
+          </Button>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
         </Toolbar>
       </Container>
     </AppBar>
