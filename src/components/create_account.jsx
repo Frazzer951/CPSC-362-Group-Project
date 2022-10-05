4bbc1fe3e4035bd0307bb02a9bbc326da9528065
Luke Eltiste <luke343279@gmail.com>
2022-10-04 17:35:37 -0700

    create account option

--- src/components/create_account.jsx
+++ src/components/create_account.jsx
@@ -1,15 +1,14 @@
-import { useState, useEffect, useContext } from "react";
-import AuthContext from "../context/AuthProvider";
+import { useState, useEffect } from "react";
 
 import axios from "../api/axios";
 const CREATE_ACCOUNT_URL = "/user/create";
 
+/// Code inspired tutorial at https://www.youtube.com/watch?v=X3qyxo_UTR4
 export default function CreateAccount() {
-  const { setAuth } = useContext(AuthContext);
-
   const [user, setUser] = useState("");
   const [pwd, setPwd] = useState("");
   const [errMsg, setErrMsg] = useState("");
+  const [sucMsg, setSucMsg] = useState("");
 
   useEffect(() => {
     setErrMsg("");
@@ -33,13 +32,15 @@ export default function CreateAccount() {
 
       setUser("");
       setPwd("");
+      setSucMsg("Account Successfully Created");
+
+      console.log(sucMsg);
     } catch (err) {
+      setSucMsg("");
       if (!err?.response) {
         setErrMsg("No Server Response");
-      } else if (err.response?.status === 400) {
-        setErrMsg("Missing Username or Password");
-      } else if (err.response?.status === 401) {
-        setErrMsg("Unauthorized");
+      } else if (err.response?.status === 406) {
+        setErrMsg("User already exists");
       } else {
         setErrMsg("Login Failed");
       }
@@ -49,6 +50,7 @@ export default function CreateAccount() {
   return (
     <section>
       <p className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
+      <p className={sucMsg ? "sucmsg" : "offscreen"}>{sucMsg}</p>
 
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username:</label>
