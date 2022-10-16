0164fd4f8ea64056ac7581def6f1bb784bed810d
Luke Eltiste <luke343279@gmail.com>
2022-10-16 15:14:23 -0700

    create posts

---
+++ src/components/create_post.jsx
@@ -0,0 +1,56 @@
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { useContext, useState } from "react";
+
+import axios from "../api/axios";
+import AuthContext from "../context/AuthProvider";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "white",
+  p: 4,
+  borderRadius: 3,
+};
+
+export default function CreatePost(props) {
+  let { threadID } = props;
+  const { auth } = useContext(AuthContext);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const onTitleChange = (e) => setTitle(e.target.value);
+  const onBodyChange = (e) => setBody(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(`Title: ${title}`);
+    console.log(`Body: ${body}`);
+
+    axios
+      .post(`/posts/${threadID}?user_id=${auth.userID}&title=${title}&body=${body}`)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  return (
+    <Container sx={style} maxWidth="sm">
+      <Box sx={{ display: "grid" }}>
+        <Typography variant="h3">Create Post</Typography>
+
+        <TextField onChange={onTitleChange} value={title} label={"Title"} variant="outlined" margin="dense" required />
+        <TextField onChange={onBodyChange} value={body} label={"Body"} variant="outlined" margin="dense" required />
+
+        <Button onClick={handleSubmit} variant="contained">
+          Create Thread
+        </Button>
+      </Box>
+    </Container>
+  );
+}
