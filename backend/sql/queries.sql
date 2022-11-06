fa424ac7f35fe4d92c812110f7b283694f7c57d6
Luke Eltiste <luke343279@gmail.com>
2022-11-06 14:12:45 -0800

    remake database and move around sql files

---
+++ backend/sql/queries.sql
@@ -0,0 +1,15 @@
+SELECT * FROM Users;
+
+SELECT * FROM Threads;
+
+SELECT * FROM Posts;
+
+SELECT * FROM Comments;
+
+DROP TABLE Users;
+
+DROP TABLE Threads;
+
+DROP TABLE Posts;
+
+DROP TABLE Comments;
